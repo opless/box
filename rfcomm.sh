0062e9b4c40c6f41150aa721965b06becf2bce6a
Simon Waite <opless@gmail.com>
2019-09-19 02:36:10 +0100

    bluetooth serial console

---
+++ rfcomm.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+/usr/bin/rfcomm -S watch hci0 1 /sbin/mgetty -r /dev/rfcomm0
+# mgetty ensures that you have a correct pty setup for /bin/login and job control in bash
+# Also check out /etc/issue.mgetty and /etc/mgetty/* for configuring mgetty.

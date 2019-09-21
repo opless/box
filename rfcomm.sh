690f9318792e5920848d4dabb772f9406b180d29
Simon Waite <opless@gmail.com>
2019-09-21 22:53:57 +0100

    added bluetooth pan network, using systemd

--- rfcomm.sh
+++ rfcomm.sh
@@ -2,3 +2,4 @@
 /usr/bin/rfcomm -S watch hci0 1 /sbin/mgetty -r /dev/rfcomm0
 # mgetty ensures that you have a correct pty setup for /bin/login and job control in bash
 # Also check out /etc/issue.mgetty and /etc/mgetty/* for configuring mgetty.
+# rfcomm -S ensures a secure link.
